8a711aade70736db14c1d5d9d80ecc8f9ecad16a
Olga R <pepsitrash@mail.ru>
2023-08-25 01:28:32 +0300

    ПР13, исправлено по комментариям

--- controllers/users.js
+++ controllers/users.js
@@ -40,54 +40,110 @@ module.exports.createUser = (req, res) => {
     });
 };
 
+// module.exports.editUserData = (req, res) => {
+//   const { name, about } = req.body;
+//   if (req.user._id) {
+//     User.findByIdAndUpdate(
+//       req.user._id,
+//       { name, about },
+//       {
+//         new: true,
+//         runValidators: true,
+//       },
+//     )
+//       .then((user) => res.send(user))
+//       .catch((err) => {
+//         if (err.name === 'ValidationError') {
+//           res.status(400).send({ message: err.message });
+//         } else {
+//           res.status(500).send({ message: 'На сервере произошла ошибка' });
+//         }
+//       });
+//   } else {
+//     res
+//       .status(404)
+//       .send({ message: 'Пользователь по указанному _id не найден' });
+//   }
+// };
+
 module.exports.editUserData = (req, res) => {
   const { name, about } = req.body;
-  if (req.user._id) {
-    User.findByIdAndUpdate(
-      req.user._id,
-      { name, about },
-      {
-        new: true,
-        runValidators: true,
-      },
-    )
-      .then((user) => res.send(user))
-      .catch((err) => {
-        if (err.name === 'ValidationError') {
-          res.status(400).send({ message: err.message });
-        } else {
-          res.status(500).send({ message: 'На сервере произошла ошибка' });
-        }
-      });
-  } else {
-    res
-      .status(404)
-      .send({ message: 'Пользователь по указанному _id не найден' });
-  }
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Передан некорректный _id' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
+
+// module.exports.editUserAvatar = (req, res) => {
+//   const { avatar } = req.body;
+//   if (req.user._id) {
+//     User.findByIdAndUpdate(
+//       req.user._id,
+//       { avatar },
+//       {
+//         new: true,
+//         runValidators: true,
+//       },
+//     )
+//       .then((user) => res.send(user))
+//       .catch((err) => {
+//         if (err.name === 'ValidationError') {
+//           res.status(400).send({ message: err.message });
+//         } else {
+//           res.status(500).send({ message: 'На сервере произошла ошибка' });
+//         }
+//       });
+//   } else {
+//     res
+//       .status(404)
+//       .send({ message: 'Пользователь по указанному _id не найден' });
+//   }
+// };
 
 module.exports.editUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  if (req.user._id) {
-    User.findByIdAndUpdate(
-      req.user._id,
-      { avatar },
-      {
-        new: true,
-        runValidators: true,
-      },
-    )
-      .then((user) => res.send(user))
-      .catch((err) => {
-        if (err.name === 'ValidationError') {
-          res.status(400).send({ message: err.message });
-        } else {
-          res.status(500).send({ message: 'На сервере произошла ошибка' });
-        }
-      });
-  } else {
-    res
-      .status(404)
-      .send({ message: 'Пользователь по указанному _id не найден' });
-  }
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Передан некорректный _id' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
