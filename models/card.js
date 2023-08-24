a81807238152fe0ced43f55c1eeed8c00b132800
Olga R <pepsitrash@mail.ru>
2023-08-24 14:31:22 +0300

    ПР13

--- models/card.js
+++ models/card.js
@@ -15,11 +15,11 @@ const cardSchema = new mongoose.Schema(
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       required: [true],
-      ref: 'user',
+      // ref: 'user',
     },
     likes: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
+      // ref: 'user',
       default: [],
     },
     createdAt: {
