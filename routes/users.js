0c1fc6c451c888851b17e21592159ca112a86c22
Olga R <pepsitrash@mail.ru>
2023-09-02 17:16:58 +0300

    ПР14, исправлено по комментариям

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { regUrl } = require('../utils/constants');
+const regUrl = require('../utils/constants');
 
 const {
   getUsers,
